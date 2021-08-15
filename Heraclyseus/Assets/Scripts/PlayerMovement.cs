36d684c0d8ab31bdc6a939608b795d6777745f58
Bryan-Huke <bhuke@outlook.com>
2021-08-15 17:48:48 -0600

    Worked on attack cooldowns
    +Added multiple cooldown timers and other related variables
    +Prevented actions from being taken while they were on cooldown
    +Began adding an attack but stopped because enemies are now needed

--- Heraclyseus/Assets/Scripts/PlayerMovement.cs
+++ Heraclyseus/Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,29 @@ public class PlayerMovement : MonoBehaviour
 
     private PlayerControls playerControls;
 
-    private float moveSpeed = 10f;
-    private float dashSpeed = 50f;
+    public Transform attackPos;
+
+    //player stats
+    public float moveSpeed = 10f;
+    public float dashSpeed = 50f;
+    public float attackAOE = 20f;
+
+    //timer to prevent multiple actions from happening at once
+    private float actionTimer = 0f;
+    public float attackDuration = 0.3f;
+    public float specialDuration = 0.7f;
+    public float pushDuration = 0.3f;
+    public float dashDuration = 0.2f;
+
+    //cooldowns for moves that cannot be spammed
+    public float specialCooldownLength = 2f;
+    public float dashCooldownLength = 1f;
+    private float specialCooldownTimer = 0f;
+    private float dashCooldownTimer = 0f;
+
+    //other variables
+    private bool canMove = true;
+
 
     private void Awake()
     {
@@ -25,70 +46,146 @@ public class PlayerMovement : MonoBehaviour
         playerControls.Disable();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-
         HandleMovement();
         HandleAttacks();
+        HandleTimers();
+
 
     }
 
+
+    /// <summary>
+    /// Takes care of player movement including dashing
+    /// </summary>
     private void HandleMovement()
-    { 
-        Vector2 movement = playerControls.Combat.Move.ReadValue<Vector2>();
-        Vector3 currentPosition = transform.position;
-
-        float dashMultiplier = 1;
-
-        if (playerControls.Combat.Dash.triggered)
+    {
+        if (canMove)
         {
-            Debug.Log("Dash");
-            dashMultiplier = dashSpeed;
+            Vector2 movement = playerControls.Combat.Move.ReadValue<Vector2>();
+            Vector3 currentPosition = transform.position;
+
+            //I am currently treating the dash as a teleport forward. I believe this will work fine
+            //but will need some work to look good and it also needs a few Iframes to avoid jankyness.
+            float dashMultiplier = 1;
+
+            if (playerControls.Combat.Dash.triggered && actionTimer <= 0 && dashCooldownTimer <= 0)
+            {
+                Debug.Log("Dash");
+                dashMultiplier = dashSpeed;
+
+                actionTimer = dashDuration;
+                dashCooldownTimer = dashCooldownLength;
+            }
+
+
+            currentPosition.x += movement.x * moveSpeed * dashMultiplier * Time.deltaTime;
+            currentPosition.y += movement.y * moveSpeed * dashMultiplier * Time.deltaTime;
+
+            transform.position = currentPosition;
+
         }
-
-        currentPosition.x += movement.x * moveSpeed * dashMultiplier * Time.deltaTime;
-        currentPosition.y += movement.y * moveSpeed * dashMultiplier * Time.deltaTime;
-
-        transform.position = currentPosition;
     }
 
+    /// <summary>
+    /// Takes care of reading attack inputs and calling the correct method
+    /// </summary>
     private void HandleAttacks()
     {
-        if (playerControls.Combat.Special.triggered)
+        if(actionTimer <= 0)
         {
-            Special();
+            if (playerControls.Combat.Special.triggered)
+            {
+                Special();
+            }
+            else if (playerControls.Combat.Attack.triggered)
+            {
+                Attack();
+            }
+            else if (playerControls.Combat.Push.triggered)
+            {
+                Push();
+            }
+
         }
-        else if (playerControls.Combat.Attack.triggered)
-        {
-            Attack();
-        }
-        else if (playerControls.Combat.Push.triggered)
-        {
-            Push();
-        }
+
     }
 
+    /// <summary>
+    /// Takes care of decrementing any of the active cooldown timers
+    /// </summary>
+    private void HandleTimers()
+    {
+        if(actionTimer > 0)
+        {
+            actionTimer -= Time.deltaTime;
+        }
+
+        if(dashCooldownTimer > 0)
+        {
+            dashCooldownTimer -= Time.deltaTime;
+        }
+
+        if(specialCooldownTimer > 0)
+        {
+            specialCooldownTimer -= Time.deltaTime;
+        }
+
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
     private void Attack()
     {
+
         Debug.Log("attack");
+
+        Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackAOE);
+        //loop and damage enemies in the array
+
+        actionTimer = attackDuration;
+
+
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     private void Special()
     {
-        Debug.Log("special");
+        if(specialCooldownTimer <= 0)
+        {
+            Debug.Log("special");
+
+            //special attack here
+
+
+            actionTimer = specialDuration;
+            specialCooldownTimer = specialCooldownLength;
+        }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     private void Push()
     {
         Debug.Log("push");
+
+        //push attack here
+
+
+        actionTimer = pushDuration;
     }
 
+    //drawing hitboxes to help visualize them during development
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackAOE);
+    }
 
 }
